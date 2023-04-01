8eb2cfc857e2a539b187e5cd4d7464c6c405f976
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-04-01 23:24:22 +0530

    remove redundant code
    prevents dependecy cycle

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -3,5 +3,3 @@ export * from "@/pages/dashboard/profile";
 export * from "@/pages/dashboard/tables";
 export * from "@/pages/dashboard/notifications";
 export * from "@/pages/dashboard/product";
-export * from "@/pages/dashboard/modals/addProduct"
-export * from "@/pages/dashboard/modals/changeProduct"
