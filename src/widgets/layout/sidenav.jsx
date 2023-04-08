925afb017503b80bce7b03c9ff87ed39c1632e96
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-04-08 22:40:00 +0530

    update brand name

--- src/widgets/layout/sidenav.jsx
+++ src/widgets/layout/sidenav.jsx
@@ -100,7 +100,7 @@ export function Sidenav({ brandImg, brandName, routes }) {
 
 Sidenav.defaultProps = {
   brandImg: "/img/logo-ct.png",
-  brandName: "Material Tailwind React",
+  brandName: "Pragati",
 };
 
 Sidenav.propTypes = {
