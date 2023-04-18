509a4c34db6bef2476f3a1bae92ec29281ee701a
shashank-iter <pandeyshashank812@gmail.com>
2023-04-19 00:41:52 +0530

    invoicedata.js added for fake invoice data

---
+++ src/data/invoicedata.js
@@ -0,0 +1,17 @@
+export const invoicedata = [
+    {
+      orderid: "5896456",
+      productName: "New Balance Shoes",
+      customerName: "Shashank Pandey",
+      date: "13/04/2023",
+    },
+    {
+      img: "/img/team-2.jpeg",
+      productName: "Kukabura Cricket Bat",
+      unitsInStock: 42,
+      reorderLevel: 10,
+    },
+  ];
+  
+  export default inovicedata;
+  
