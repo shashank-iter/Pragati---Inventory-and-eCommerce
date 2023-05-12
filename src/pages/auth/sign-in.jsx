956fb908baa5a562ed2ea5183e5e803b330741a5
shashank-iter <pandeyshashank812@gmail.com>
2023-05-12 23:02:08 +0530

    auth.uid stored into cookies

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -37,6 +37,7 @@ export function SignIn() {
       // console.log(data.user.role);
       // console.log(data.session.access_token);
       Cookies.set("token", data.session.access_token);
+      Cookies.set("uid", data.user.id);
       Cookies.set("email", data.user.email);
       if (Cookies.get("token")) {
         window.location.href = "/dashboard/home";
