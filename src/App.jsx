f554d0e281fcc263cd6f2b5946ee19b2e78ce87a
shashank-iter <pandeyshashank812@gmail.com>
2023-05-25 01:11:16 +0530

    store route modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { Routes, Route, Navigate, useParams } from "react-router-dom";
-import { Dashboard, Auth } from "@/layouts";
-import Demo from "./pages/dashboard/Demo";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard, Auth, StorePages } from "@/layouts";
 
 function App() {
   // const key =  URLSearchParams(window.location.search);
@@ -11,6 +10,10 @@ function App() {
     <Routes>
       <Route path="/dashboard/*" element={<Dashboard />} />
       <Route path="/auth/*" element={<Auth />} />
+
+      {/* route for store pages */}
+      <Route path="/store/:key" element={<StorePages />} />
+
       {/* <Route path="/demo/*" element={<Demo />} /> */}
       <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
     </Routes>
