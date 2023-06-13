3a33f8d426418506995882d30a9db4506a555891
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2023-06-13 13:18:33 +0530

    update UI of sign in and sign up pages

--- src/pages/auth/sign-up.jsx
+++ src/pages/auth/sign-up.jsx
@@ -63,7 +63,7 @@ export function SignUp() {
       <div className="absolute inset-0 z-0 h-full w-full bg-black/50" />
       <div className="container mx-auto p-4">
         <form>
-          <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
+          <Card className="absolute top-2/4 left-2/4 mt-10 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
             <CardHeader
               variant="gradient"
               color="blue"
